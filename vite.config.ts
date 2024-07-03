ea4296b2103e2056b4f556a7ec1920e9ed505298
Pavel <pvpkiselev@gmail.com>
2024-07-03 13:05:54 +0300

    vite2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/todo',
+  base: '/todo/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
